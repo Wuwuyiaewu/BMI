4bb84b2fb27dfbcfa757a0932eb059264b441448
bbking <okman9527>
2019-04-23 11:55:42 +0800

    色票與間隔

--- js/all.js
+++ js/all.js
@@ -4,12 +4,16 @@ var hhh = document.querySelector('#height');
 var www = document.querySelector('#weight');
 var data = JSON.parse(localStorage.getItem('yourBMI')) || [];
 var check_holder = document.querySelector('.result_list');
+posting(data);
 function counting(e) {
     e.preventDefault();
     
 	var height = (hhh.value / 100) * (hhh.value / 100);
 	var weight = www.value;
-	var x = (weight / height).toFixed(2);
+	var x = (weight / height);
+	x = x.toFixed(2);
+	console.log(weight);
+	console.log(height);
 	console.log(x);
 	var txt = {
 		BMI:x,
@@ -23,22 +27,34 @@ function counting(e) {
 function posting(data){
 	var str = "";
 	var eva = "";
+	var color = "";
 	for (var i = 0; i < data.length; i++) {
 		if (data[i].BMI < 18.5) {
-			eva = "體重過輕"
+			eva = "體重過輕";
+			color = "color_1";
 		}else if (data[i].BMI > 18.5 && data[i].BMI <= 25) {
 			eva = "體重正常"
+			color = "color_2";
+
 		}else if (data[i].BMI > 25 && data[i].BMI <= 30){
 			eva = "體重過重"
+			color = "color_3";
+
 		}else if (data[i].BMI > 30 && data[i].BMI <= 35){
 			eva = "中等肥胖"
+			color = "color_4";
+
 		}else if (data[i].BMI > 35 && data[i].BMI <= 40){
 			eva = "嚴重肥胖"
+			color = "color_5";
+
 		}else if (data[i].BMI > 40){
 			eva = "非常嚴重肥胖"
+			color = "color_6";
+
 		};
 		str += 
-			'<li data-num="'+i+'">'+
+			'<li data-num="'+i+'" class="'+ color +'">'+
 				'<table>'+
 					'<tr>'+
 						'<td>'+eva+
@@ -57,7 +73,9 @@ function posting(data){
 	}
 	check_holder.innerHTML = str;
 }
+function del(e){
 
+}
 
 
 var con = document.querySelector('Body');
